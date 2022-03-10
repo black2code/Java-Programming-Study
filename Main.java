ac43a2122e162a9be3721b9c1ba436d491395d0c
black2code <alsdnr1080@gmail.com>
2022-03-10 23:15:48 +0900

    CodeUp1001-1008

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        //1001 ~ 1008 [기초-출력] 출력하기
+        System.out.println("Hello");
+        System.out.println("Hello World");
+        System.out.print("Hello\nWorld");
+        System.out.println("\'Hello\'");
+        System.out.println("\"Hello World\"");
+        System.out.println("\"!@#$%^&*()\"");
+        System.out.println("\"C:\\Download\\hello.cpp\"");
+        System.out.println("\u250c\u252c\u2510\n\u251c\u253c\u2524\n\u2514\u2534\u2518");
+    }
+}
