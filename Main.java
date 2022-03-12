1cd0a62efd67de4ecde69cfd020c2ffa6227fee5
black2code <alsdnr1080@gmail.com>
2022-03-12 23:56:34 +0900

    CodeUp1015-1018

--- Main.java
+++ Main.java
@@ -1,17 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         //1001 ~ 1008 [기초-출력] 출력하기
-//        System.out.println("Hello");
-//        System.out.println("Hello World");
-//        System.out.print("Hello\nWorld");
-//        System.out.println("\'Hello\'");
-//        System.out.println("\"Hello World\"");
-//        System.out.println("\"!@#$%^&*()\"");
-//        System.out.println("\"C:\\Download\\hello.cpp\"");
-//        System.out.println("\u250c\u252c\u2510\n\u251c\u253c\u2524\n\u2514\u2534\u2518");
+//        System.out.println("Hello"); 1001
+//        System.out.println("Hello World"); 1002
+//        System.out.print("Hello\nWorld"); 1003
+//        System.out.println("\'Hello\'"); 1004
+//        System.out.println("\"Hello World\""); 1005
+//        System.out.println("\"!@#$%^&*()\""); 1006
+//        System.out.println("\"C:\\Download\\hello.cpp\""); 1007
+//        System.out.println("\u250c\u252c\u2510\n\u251c\u253c\u2524\n\u2514\u2534\u2518"); 1008
 
-// 1010 ~ 1014 [기초-입출력]
-
+        // 1010 ~ 1014 [기초-입출력]
 //        int n = sc.nextInt();
 //        System.out.println(n);    1010
 
@@ -28,5 +30,16 @@ public class Main {
 //        Scanner sc = new Scanner(System.in);
 //        char ch1 = sc.next().charAt(0), ch2 = sc.next().charAt(0);
 //        System.out.println(ch2 + " " + ch1);  1014
+
+        // 1015 ~ 1018 [기초-입출력]
+//        float x = sc.nextFloat();
+//        System.out.println(String.format("%.2f",x)); 1015
+
+//        int n = sc.nextInt();
+//        System.out.println(n + " "+ n + " " + n); 1017
+//
+//        String time = sc.nextLine();
+//        System.out.println(time); 1018
+
     }
 }
