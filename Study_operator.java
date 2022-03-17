69cc8149dedcd0ccd179b3be9c443dd48a3ef775
black2code <alsdnr1080@gmail.com>
2022-03-17 23:56:48 +0900

    Study05

---
+++ Study_operator.java
@@ -0,0 +1,9 @@
+public class Study_operator {
+    public static void main(String[] args) {
+        //삼항 연산자
+        System.out.println("====삼함 연산자 ====");
+        int a = 100;
+        String aResult = (a==100) ? "yes":"no";
+        System.out.println("aResult = " + aResult);
+    }
+}
