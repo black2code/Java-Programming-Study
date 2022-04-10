d82244631abbbdc6917bb261b0ea0201a299abc7
black2code <alsdnr1080@gmail.com>
2022-04-11 03:10:33 +0900

    Quiz07

--- CodeTest/Quiz07.java
+++ CodeTest/Quiz07.java
@@ -5,12 +5,10 @@ package CodeTest;
     로또의 개수를 구매하고(구매수량 입력), 당첨번호를 생성한다. 이후, 구매한 로또의 당첨번호를 판단하는 프로그램 작성
  */
 
-
 import java.util.*;
 import java.util.stream.Stream;
 
 public class Quiz07 {
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Random random = new Random();
@@ -20,57 +18,57 @@ public class Quiz07 {
         LinkedList list4 = new LinkedList();
         int count = 0;
 
-        String [] alphabet = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J"};
+        String[] alphabet = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J"};
 
         System.out.println("[로또 당첨 프로그램]\n");
         System.out.print("로또 개수를 입력해주세요.(숫자 1 ~ 10):");
         int lcount = sc.nextInt();
 
-        for (int j = 0; j < lcount; j++) {
-            System.out.print(alphabet[j] + "\t");
-
-            for (int i = 0; lottoset.size() < 6; i++) {
-                int lnum = random.nextInt(45)+1;
+        for (int i = 0; i < lcount; i++) {
+            System.out.print(alphabet[i] + "\t");
+            for (int j = 0; lottoset.size() < 6; j++) {
+                int lnum = random.nextInt(45) + 1;
                 lottoset.add(lnum);
             }
             LinkedList list = new LinkedList(lottoset);
             Collections.sort(list);
             list3.addLast(list);
             Stream stream = list.stream();
-            stream.forEach(n -> System.out.printf("%02d ",n));
+            stream.forEach(n -> System.out.printf("%02d ", n));
             System.out.println();
             lottoset.clear();
         }
 
-
         System.out.println();
         System.out.println("[로또 발표]");
         for (int i = 0; jackpotset.size() < 6; i++) {
-            int jnum = random.nextInt(45)+1;
+            int jnum = random.nextInt(45) + 1;
             jackpotset.add(jnum);
         }
         System.out.print("\t");
         LinkedList list2 = new LinkedList(jackpotset);
         Stream stream2 = list2.stream();
-        stream2.forEach(n -> System.out.printf("%02d ",n));
+        stream2.forEach(n -> System.out.printf("%02d ", n));
         System.out.println();
-
 
         System.out.println();
         System.out.println("[내 로또 결과]");
-        for (int j = 0; j < lcount; j++) {
 
-            System.out.print(alphabet[j] + "\t");
-            list4 = (LinkedList) list3.get(j);
+        for (int i = 0; i < lcount; i++) {
+            System.out.print(alphabet[i] + "\t");
+            list4 = (LinkedList) list3.get(i);
             Stream stream3 = list4.stream();
-            stream3.forEach(n -> System.out.printf("%02d ",n));
+            stream3.forEach(n -> System.out.printf("%02d ", n));
 
-//            for (int i = 0; i < 6; i++) {
-//                if(){
-//                    count += 1;
-//                }
-//            }
+            for (int j = 0; j < 6; j++) {
+                for (int k = 0; k < 6; k++) {
+                    if (list2.get(j) == list4.get(k)){
+                        count += 1;
+                    }
+                }
+            }
             System.out.println("=>" + count +" 개 일치");
+            count = 0;
         }
     }
 }
