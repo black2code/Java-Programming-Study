9fadf6da01a470e01a3f38245e23b85f57d2807a
black2code <alsdnr1080@gmail.com>
2022-04-13 16:50:34 +0900

    Quiz05_Mod

--- CodeTest/Quiz05.java
+++ CodeTest/Quiz05.java
@@ -20,7 +20,7 @@ public class Quiz05 {
         System.out.print("달력의 월을 입력 주세요.(mm):");
         int month = sc.nextInt();
 
-        LocalDate monthstart = LocalDate.of(year,month,1);
+        LocalDate monthstart = LocalDate.of(year, month, 1);
         int monthday = monthstart.get(ChronoField.DAY_OF_WEEK); //해당 월의 첫 번째 요일 확인
         int monthend = monthstart.lengthOfMonth();  // 해당 월의 마지막일
 
@@ -29,15 +29,18 @@ public class Quiz05 {
         System.out.println("[" + year + "년 " + month + "월" + "]");
         System.out.println("일\t월\t화\t수\t목\t금\t토");
 //                          0   1  2   3  4   5  6
+        int totalCount = 0;
 
         for (int i = 0; i < monthday; i++) {
             System.out.print("\t");
+            totalCount++;
         }
-        for (int j = 1; j < monthend+1; j++) {
-            if((monthday-1+j)%7==0){
+        for (int j = 1; j <= monthend; j++) {
+            System.out.printf(String.format("%02d\t", j));
+            totalCount++;
+            if (totalCount % 7 == 0) {
                 System.out.println();
             }
-            System.out.printf(String.format("%02d\t",j));
         }
     }
 }
