c09d2bde86dba6e27ea5adcc2faac89ba4890ee3
black2code <alsdnr1080@gmail.com>
2022-04-27 20:50:21 +0900

    Math_exponentiation_Practice

---
+++ Math/exponentiation_Practice.java
@@ -0,0 +1,53 @@
+package Math;
+
+// 제곱과 제곱근을 Math 없이 구현하기
+
+public class exponentiation_Practice {
+    
+    public static double pow(int a, double b){
+        double result = 1;
+        boolean isMinus = false;
+
+        if(b == 0){ // 어떤수의 0 제곱은 1
+            return 1;
+        }else if(b <0){
+            b*=-1;
+            isMinus = true;
+        }
+        for (int i = 0; i < b; i++) {
+            result *= a;
+        }
+        return isMinus ? 1 / result : result; // 음수라면 나눠서 리턴 해주고, 아니라면 그대로 리턴
+    }
+
+    public static double sqrt(int a){ // 바빌로니아 방법
+        double result = 1;
+
+        for (int i = 0; i < 10; i++) {
+            result = (result  + (a/result))/2;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        //      Test code
+        System.out.println("== Math pow ==");
+        System.out.println(Math.pow(2, 3));
+        System.out.println(Math.pow(2, -3));
+        System.out.println(Math.pow(-2, -3));
+
+        System.out.println("== My pow ==");
+        System.out.println(pow(2, 3));
+        System.out.println(pow(2, -3));
+        System.out.println(pow(-2, -3));
+
+        System.out.println("== Math sqrt ==");
+        System.out.println(Math.sqrt(16));
+        System.out.println(Math.sqrt(8));
+
+        System.out.println("== My sqrt ==");
+        System.out.println(sqrt(16));
+        System.out.println(sqrt(8));
+
+    }
+}
