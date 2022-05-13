c2c6535631622329d218f753347518e21a4297a0
black2code <alsdnr1080@gmail.com>
2022-05-14 00:17:34 +0900

    Programmers03

---
+++ Algorithm/Programmers03.java
@@ -0,0 +1,18 @@
+package Algorithm;
+
+public class Programmers03 {
+    public static String solution(String phone_number) {
+        String answer = "";
+
+        for (int i = 0; i < phone_number.length()-4; i++) {
+            answer += "*";
+        }
+        answer += phone_number.substring(phone_number.length()-4);
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        String phone_number = "01033334444";
+        System.out.println(solution(phone_number));
+    }
+}
