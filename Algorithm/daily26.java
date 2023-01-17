92d092430dc6f4947a77b451d892754039e3c24b
black2code <alsdnr1080@gmail.com>
2023-01-17 20:55:09 +0900

    daily26

---
+++ Algorithm/daily26.java
@@ -0,0 +1,47 @@
+package Algorithm;
+
+import java.util.Scanner;
+
+public class daily26 {
+
+    public static int solution(int num, int arr[]) {
+        int answer = 1;
+        int max = arr[0];
+
+        for (int i = 0; i < num-1; i++) {
+            int left = i;
+            int right = i+1;
+
+            if (arr[left] < arr[right] && max < arr[right]) {
+                max = arr[right];
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        int arr[] = new int[num];
+        for (int i = 0; i < num; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println(solution(num, arr));
+    }
+}
+
+/*
+선생님이 N명의 학생을 일렬로 세웠습니다. 일렬로 서 있는 학생의 키가 앞에서부터 순서대로 주어질 때, 맨 앞에 서 있는
+
+선생님이 볼 수 있는 학생의 수를 구하는 프로그램을 작성하세요. (앞에 서 있는 사람들보다 크면 보이고, 작거나 같으면 보이지 않습니다.)
+
+8
+130 135 148 140 145 150 150 153
+
+정답 5
+
+ */
